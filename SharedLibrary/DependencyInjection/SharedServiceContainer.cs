cf230f163540e7b09d7dfd4717a5bd0f33a3248b
MrEshboboyev <mreshboboyev@gmail.com>
2024-10-24 16:48:20 +0500

    DependencyInjection/SharedServiceContainer.cs

---
+++ SharedLibrary/DependencyInjection/SharedServiceContainer.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using SharedLibrary.Middleware;
+
+namespace SharedLibrary.DependencyInjection;
+
+public static class SharedServiceContainer
+{
+    public static IServiceCollection AddSharedService<TContext>
+        (this IServiceCollection services, IConfiguration config, string fileName) where TContext : DbContext
+    {
+        // Add Generic Database context
+        services.AddDbContext<TContext>(option =>
+        {
+            option.UseNpgsql(config.GetConnectionString("PostgresConnection"));
+        });
+        // configure serilog logging
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Information()
+            .WriteTo.Debug()
+            .WriteTo.Console()
+            .WriteTo.File(
+                path: $"{fileName}-.text",
+                restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information,
+                outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {message:lj}{NewLine}{Exception}",
+                rollingInterval: RollingInterval.Day)
+            .CreateLogger();
+        // Add JWT Authentication Scheme 
+        JWTAuthenticationScheme.AddJWTAuthenticationScheme(services, config);
+        return services;
+    }
+    public static IApplicationBuilder UseSharedPolicies(this IApplicationBuilder app)
+    {
+        // Use Global Exception
+        app.UseMiddleware<GlobalException>();
+        // Register Middleware to block all outsiders API calls
+        app.UseMiddleware<ListenToOnlyApiGateway>();
+        return app;
+    }
+}
