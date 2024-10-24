dabf87beb86de704720094286597fa9905e2b267
MrEshboboyev <mreshboboyev@gmail.com>
2024-10-24 16:40:28 +0500

    Middleware/ListenToOnlyApiGateway.cs

---
+++ SharedLibrary/Middleware/ListenToOnlyApiGateway.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace SharedLibrary.Middleware;
+
+public class ListenToOnlyApiGateway(RequestDelegate next)
+{
+    public async Task InvokeAsync(HttpContext context)
+    {
+        // Extract specific header form Request
+        var signedHeader = context.Request.Headers["Api-Gateway"];
+        // NULL means, the request is not coming from the Api Gateway // 503 service unaavailable
+        if (signedHeader.FirstOrDefault() is null)
+        {
+            context.Response.StatusCode = StatusCodes.Status503ServiceUnavailable;
+            await context.Response.WriteAsync("Sorry, service is unavailable!");
+            return;
+        }
+        else
+        {
+            await next(context);
+        }
+    }
+}
+
